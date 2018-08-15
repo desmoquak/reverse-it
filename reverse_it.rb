9aec720fc9952a2ecdfdaf7b04a17838c1f7a73e
John Schaub <schaubjs@gmail.com>
2018-08-14 21:31:24 -0700

    Add files via upload

---
+++ reverse_it.rb
@@ -0,0 +1,13 @@
+#Write a method that takes one argument, a string, and returns the same string with the words in reverse order.
+
+s = "hello world"
+puts s.reverse
+puts s
+
+s = "hello world"
+puts s != s.reverse
+
+# Output
+#dlrow olleh
+#hello world
+#true
